f1378b80f40beca0f5b54e93a51a1b2f03d86f3f
Bryan Baker <6157sound@gmail.com>
2023-12-28 12:20:31 -0600

    added pages dir

---
+++ baker-rr-website/src/pages/test.tsx
@@ -0,0 +1,18 @@
+// pages/example.tsx
+
+import React from "react";
+
+interface ExampleProps {
+  // Define your component props here
+}
+
+const Example: React.FC<ExampleProps> = () => {
+  return (
+    <div>
+      <h1>Example Page</h1>
+      <p>This is a simple Next.js page template with TypeScript.</p>
+    </div>
+  );
+};
+
+export default Example;
