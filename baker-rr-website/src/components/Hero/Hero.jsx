8bc0c60e8ccaf1cf63bd7469e9f3e185e5d80492
Bryan Baker <6157sound@gmail.com>
2023-12-28 17:24:09 -0600

    fix typo

--- baker-rr-website/src/components/Hero/Hero.jsx
+++
@@ -1,7 +0,0 @@
-import Image from "next/image";
-import hero from "../../../public/images/hero.jpeg";
-import styles from "./_Hero.module.scss";
-
-export default function Hero() {
-  return <div className={styles.hero}>hello</div>;
-}
