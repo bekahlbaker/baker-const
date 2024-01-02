e56529ac94352cd3b9eab31b39f32ead8eb6485f
Bekah Baker <bekahlbaker@gmail.com>
2024-01-01 20:59:53 -0600

    update header logo

--- baker-rr-website/src/app/components/Header/Header.jsx
+++ baker-rr-website/src/app/components/Header/Header.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import styles from "./Header.module.scss";
 import Container from "../Container/Container";
+import Logo from "../../../../public/images/BakerRRLogo.png"
+import Image from "next/image";
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <Container>
-        <img src="https://place-hold.it/180x68/3B3B98/ffffff&text=BakerRR&bold&fontsize=22" />
-      </Container>
+        <Container>
+          <Image src={Logo} alt="Baker Remodel & Restoration Logo" />
+        </Container>
     </header>
   );
 };
