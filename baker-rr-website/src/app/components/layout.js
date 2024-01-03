5aa65c458ddae9f705d87b578faa959b6779fb29
Bryan Baker <bryan.baker@ascension-external.org>
2024-01-03 14:11:38 -0600

    added SpeedInsights

--- baker-rr-website/src/app/components/layout.js
+++ baker-rr-website/src/app/components/layout.js
@@ -2,6 +2,7 @@ import { Saira } from "next/font/google";
 import "../globals.css";
 import Footer from "./Footer/Footer";
 import Header from "./Header/Header";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Saira({ subsets: ["latin"] });
 
@@ -9,6 +10,7 @@ export default function Layout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <SpeedInsights />
         <Header />
         {children}
         <Footer />
